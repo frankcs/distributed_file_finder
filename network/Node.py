b2c378b1a830bebae9bdedbba1eb0ecb9a6bdd7b
Jorge Tomas <jotolo7@gmail.com>
2013-01-10 00:30:55 -0500

    cambios en los bind de los sockets

--- network/Node.py
+++ network/Node.py
@@ -268,10 +268,12 @@ class Node(threading.Thread):
             self.mySocket.bind((str(self.myIp),ANSWERPORT))
             #self.timer=Timer(3.0,self.print_death)
             #self.timer.start()
+            msg1=None
             try:
                 (msg1, address) = self.mySocket.recvfrom(65536)
             except :
                 self.print_death()
+                return False
             #self.timer.cancel()
             self.mySocket.close()
             if msg1.decode() == "YES":
@@ -285,13 +287,16 @@ class Node(threading.Thread):
             sock_out.sendto(msg.encode(), (str(self.childAdrr), PORT))
             self.mySocket =  socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             self.mySocket.settimeout(3.0)
+            print("bind")
             self.mySocket.bind((str(self.myIp),ANSWERPORT))
             #self.timer=Timer(3.0,self.print_death)
             #self.timer.start()
+            msg1=None
             try:
                 (msg1, address) = self.mySocket.recvfrom(65536)
             except :
                 self.print_death()
+                return False
             #self.timer.cancel()
             self.mySocket.close()
             if msg1.decode() == "YES":
